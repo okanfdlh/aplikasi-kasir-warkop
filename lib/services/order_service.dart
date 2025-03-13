681ecaade90612c7e7e395bacd8dbb24e3220ab4
okanfdlh <ignadeafiranti@gmail.com>
2025-03-14 00:00:19 +0700

    Update order_service.dart

--- lib/services/order_service.dart
+++ lib/services/order_service.dart
@@ -3,7 +3,7 @@ import 'package:http/http.dart' as http;
 
 class OrderService {
   static Future<List<dynamic>> fetchOrders() async {
-    final response = await http.get(Uri.parse('http://192.168.1.4:8000/api/orders'));
+    final response = await http.get(Uri.parse('https://seduh.dev-web2.babelprov.go.id/api/orders'));
 
     if (response.statusCode == 200) {
       return json.decode(response.body);
